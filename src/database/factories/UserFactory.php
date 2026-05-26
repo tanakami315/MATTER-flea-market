c8b815d557be78592f41aa5ff1e2809bf4ac4772
tanakami <m.tanakami.a@gmail.com>
2026-05-26 14:04:45 +0900

    20260526_1400_Userデータ修正

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -18,7 +18,10 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'postal_code' => $this->faker->regexify('[0-9]{3}-[0-9]{4}'),
+            'address' => $this->faker->prefecture() . $this->faker->city() . $this->faker->streetAddress(),
+            'building_name' => $this->faker->secondaryAddress(),
+            'password' => bcrypt('password'), // password
             'remember_token' => Str::random(10),
         ];
     }
